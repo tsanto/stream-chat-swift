b05997b3247b8c698ec1932aca35daecfb70e8f2
Dominik Bucher <dominik@getstream.io>
2020-11-26 02:17:24 +0100

    Add ability to scroll and see part of screen

--- Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListCollectionViewCell.swift
+++ Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListCollectionViewCell.swift
@@ -7,15 +7,43 @@ import UIKit
 
 open class ChatChannelListCollectionViewCell<ExtraData: UIExtraDataTypes>: UICollectionViewCell {
     // MARK: - Properties
-    
+
     var uiConfig: UIConfig<ExtraData> = .default
-    
+
+    private let scrollView: UIScrollView = {
+            let scrollView = UIScrollView(frame: .zero)
+            scrollView.isPagingEnabled = true
+            scrollView.showsVerticalScrollIndicator = false
+            scrollView.showsHorizontalScrollIndicator = false
+            return scrollView
+    }()
+
     public private(set) lazy var channelView: ChatChannelListItemView<ExtraData> = {
+        let width = UIApplication.shared.keyWindow!.bounds.width
         let view = uiConfig.channelList.channelListItemView.init(uiConfig: uiConfig)
-        contentView.embed(view)
+        view.pin(anchors: [.width], to: width)
+        let stackView = UIStackView()
+        stackView.axis = .horizontal
+        stackView.distribution = .fill
+        stackView.addArrangedSubview(view)
+        stackView.addArrangedSubview(deleteView)
+
+        contentView.embed(scrollView)
+        contentView.backgroundColor = .blue
+        scrollView.embed(stackView)
+        stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
+        stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 1.2).isActive = true
         return view
     }()
-    
+
+    public private(set) lazy var deleteView: UIButton = {
+        let view = UIButton()
+        let width = UIApplication.shared.keyWindow!.bounds.width
+        view.pin(anchors: [.width], to: width / 5)
+        view.backgroundColor = .blue
+        return view
+    }()
+
     // MARK: - Layout
     
     override open func preferredLayoutAttributesFitting(
