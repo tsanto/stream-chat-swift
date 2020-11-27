7bacbb985e8d09d6f4b3a3e30fbe2433b7db8dba
Dominik Bucher <dominik@getstream.io>
2020-11-27 17:58:59 +0100

    Add swiping feature to cell

--- Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListCollectionViewCell.swift
+++ Sources_v3/StreamChatUI/ChatChannelList/ChatChannelListCollectionViewCell.swift
@@ -11,11 +11,11 @@ open class ChatChannelListCollectionViewCell<ExtraData: UIExtraDataTypes>: UICol
     var uiConfig: UIConfig<ExtraData> = .default
 
     private let scrollView: UIScrollView = {
-            let scrollView = UIScrollView(frame: .zero)
-            scrollView.isPagingEnabled = true
-            scrollView.showsVerticalScrollIndicator = false
-            scrollView.showsHorizontalScrollIndicator = false
-            return scrollView
+        let scrollView = UIScrollView(frame: .zero)
+        scrollView.isPagingEnabled = true
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+        return scrollView
     }()
 
     public private(set) lazy var channelView: ChatChannelListItemView<ExtraData> = {
@@ -29,7 +29,7 @@ open class ChatChannelListCollectionViewCell<ExtraData: UIExtraDataTypes>: UICol
         stackView.addArrangedSubview(deleteView)
 
         contentView.embed(scrollView)
-        contentView.backgroundColor = .blue
+        contentView.backgroundColor = .yellow
         scrollView.embed(stackView)
         stackView.heightAnchor.constraint(equalTo: scrollView.heightAnchor).isActive = true
         stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, multiplier: 1.2).isActive = true
@@ -37,12 +37,21 @@ open class ChatChannelListCollectionViewCell<ExtraData: UIExtraDataTypes>: UICol
     }()
 
     public private(set) lazy var deleteView: UIButton = {
-        let view = UIButton()
+        let button = UIButton()
+        if #available(iOS 13.0, *) {
+            button.setImage(UIImage(systemName: "trash"), for: .normal)
+        }
         let width = UIApplication.shared.keyWindow!.bounds.width
-        view.pin(anchors: [.width], to: width / 5)
-        view.backgroundColor = .blue
-        return view
+        button.addTarget(self, action: #selector(handleCellDelete), for: .touchUpInside)
+        // Multiplier for stackview width is 1.2 so it's 1/5 of the screen now.
+        button.pin(anchors: [.width], to: width / 5)
+        button.backgroundColor = .red
+        return button
     }()
+
+    // MARK: - Actions
+
+    @objc func handleCellDelete() {}
 
     // MARK: - Layout
     
