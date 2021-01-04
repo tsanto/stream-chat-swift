7762014d6b7012b0ba7c95c2d293a7308de0050b
Nuno Vieira <nunovieira@Nunos-Mac-mini.home>
2021-01-04 17:35:03 +0000

    Create SwiftUI app

---
+++ iMessageClone/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  iMessageClone
+//
+//  Created by Nuno Vieira on 04/01/2021.
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iMessageClone/iMessageCloneApp.swift
@@ -0,0 +1,18 @@
+//
+//  iMessageCloneApp.swift
+//  iMessageClone
+//
+//  Created by Nuno Vieira on 04/01/2021.
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct iMessageCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
