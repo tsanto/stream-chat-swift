506d890a10a80471e37b2699dfe649d29c58bb74
Dominik Bucher <d.bucher@centrum.cz>
2021-01-22 15:11:29 +0100

    Add configuration + sample

--- SwiftUI_Demo/ContentView.swift
+++ SwiftUI_Demo/ContentView.swift
@@ -11,8 +11,29 @@ import StreamChat
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Chat(
+            client: ChatClient(
+                config: ChatClientConfig(apiKey: APIKey(Configuration.apiKey)),
+                tokenProvider: .anonymous
+            )
+        )
+    }
+}
+/// Shows the list of channels when the current user is a member
+struct Chat: View {
+    @StateObject var channelList: ChatChannelListController.ObservableObject
+
+    init(client: ChatClient) {
+        channelList = client.channelListController(query: .init(filter: .containMembers(userIds: ["UserId"]))).observableObject
+    }
+
+    var body: some View {
+        List {
+            ForEach(channelList.channels) { channel in
+                Text(channel.name)
+            }
+
+        }
     }
 }
 
