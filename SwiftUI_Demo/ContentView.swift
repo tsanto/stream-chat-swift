41d8c1f9a0322d7b3550d141b38e18053b047d10
Dominik Bucher <d.bucher@centrum.cz>
2021-01-22 14:31:17 +0100

    Add SwiftUI_Demo

---
+++ SwiftUI_Demo/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUI_Demo
+//
+//  Created by Dominik Bucher on 22.01.2021.
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import SwiftUI
+import StreamChat
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Demo/SwiftUI_DemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUI_DemoApp.swift
+//  SwiftUI_Demo
+//
+//  Created by Dominik Bucher on 22.01.2021.
+//  Copyright © 2021 Stream.io Inc. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
